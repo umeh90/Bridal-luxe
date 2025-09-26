166e3f26a7019b8ca3dff4f443ffc8816c17f5e6
Jennifer <ifeomaumeh090@gmail.com>
2025-09-26 13:48:05 +0100

    Bridal luxe Original

---
+++ bridal.js
@@ -0,0 +1,11 @@
+
+const burger =document.querySelector('.hamburger');
+const nav=document.querySelector('.nav-links');
+burger.addEventListener('click', ()=>{
+	nav.classList.toggle('open');
+	});
+
+
+
+
+
---
+++ darkmode.js
@@ -0,0 +1,4 @@
+const toggleButton=document.getElementById('dark-mode-toggle');
+toggleButton.addEventListener('click', ()=>{
+	document.body.classList.toggle('dark-mode');
+});
